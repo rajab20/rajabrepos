709161ed38d96b8aee2611ef5ff55ca101859b39
Rajab Mailov <rajabmailov5@gmail.com>
2023-11-27 03:37:18 +0100

    product app created, multilanguage added

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,15 +1,22 @@
 from django.conf import settings
+from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += i18n_patterns(
     path('', include('core.urls')),
     path('', include('account.urls')),
     path('', include('about.urls')),
     path('', include('contact.urls')),
-]
+    # path('', include('product.urls')),
+    path('i18n/', include('django.conf.urls.i18n')),
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
--- app/core/views.py
+++ app/core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from contact.models import ContactInfo
 
 def index(request):
     return render(request, 'home/index.html')
